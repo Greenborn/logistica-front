ec4b67cfdc354c0c9a51ff742ae5eaaf4e635561
debian <lucho.2012.tandil@gmail.com>
2020-09-24 18:33:17 -0300

    [DEV] Se agrega validación en vista de edición y creación, [MOD] Se modifica la vista de remitos para hacerla más atractiva visualmente, se mueve para arriba selector de sucursal (form envio), Se reduce la coantidad de peticiones de datos elegibles en formulario, se limpia el PUT de envios

--- src/app/models/shipping.ts
+++ src/app/models/shipping.ts
@@ -9,6 +9,7 @@ export class Shipping {
     public sender_identification:any = { "type": 1, "value" : "" };
     public receiver_identification:any = { "type": 1, "value" : "" };
     public items:any = [];
+    public price;
 
     public distance_id:number;
     public vehicle_id:number;
@@ -17,11 +18,40 @@ export class Shipping {
     public destination_branch_office;
     public vehicle;
 
-    public price;
-    public remitos:any;
-    public originBranchOffice;
     public payment_at_origin:any = true;
 
     public date;
     public status;
+
+    public setValuesFromResponse( response:any ){
+      this.sender_identification.type    = response.sender_identification.identification_type.id;
+      this.receiver_identification.type  = response.receiver_identification.identification_type.id;
+      this.destination_branch_office     = response.destinationBranchOffice.id;
+      this.service_type_id               = response.serviceType.id;
+      this.distance_id                   = response.distance.id;
+      this.shipping_type_id              = response.shippingType.id;
+      this.status                        = response.status.id;
+
+      this.origin_full_name      = response.origin_full_name;
+      this.origin_contact        = response.origin_contact;
+      this.id                    = response.id;
+      this.destination_full_name = response.destination_full_name;
+      this.destination_contact   = response.destination_contact;
+      this.origin_address        = response.origin_address;
+      this.destination_address   = response.destination_address;
+      this.price                 = response.price;
+      this.date                  = response.date;
+
+      this.sender_identification.value   = response.sender_identification.value;
+      this.receiver_identification.value = response.receiver_identification.value;
+
+      if ( response.vehicle != null ){
+        this.vehicle_id = response.vehicle.id;
+      }
+
+      for( let c=0; c < response.shippingItems.length; c++ ){
+        this.items.push( { description: response.shippingItems[ c ].item, id: response.shippingItems[ c ].id } );
+      }
+
+    }
 }
